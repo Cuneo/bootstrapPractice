f796321a34bc22dcb04f894bda851088cc4a4c87
Dominic Cuneo <cuneo94@gmail.com>
2016-01-28 16:38:58 -0700

    added text

--- bootstrap.php
+++ bootstrap.php
@@ -35,13 +35,60 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
+					<nav class="navbar navbar-default">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="#">Brand</a>
+							</div>
+
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+									<li><a href="#">Link</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Separated link</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">One more separated link</a></li>
+										</ul>
+									</li>
+								</ul>
+								<form class="navbar-form navbar-left" role="search">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Search">
+									</div>
+									<button type="submit" class="btn btn-default">Submit</button>
+								</form>
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="#">Link</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Separated link</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</nav>
 			<header> header </header>
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
 				</div>
 			<div class="row">
 				<div class="col-md-12"
@@ -71,6 +118,7 @@
 					</div>
 				<div class="row">
 					<div class="col-md-12".col-md-12>
+						<p>you made the hard choice. Talk about going nowhere fast. This is not about revenge. This is about justice. Well, I'll say this for him - he's sure of himself. Ensign Babyface! Worf, It's better than music. It's jazz. You enjoyed that. I will obey your orders. I will serve this ship as First Officer. And in an attack against the Enterprise, I will die with this crew. But I will not break my oath of loyalty to Starfleet. I guess it's better to be lucky than good. The unexpected is our normal routine. I am your worst nightmare! What? We're not at all alike! You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? When has justice ever been as simple as a rule book? Maybe we better talk out here; the observation lounge has turned into a swamp. Some days you get the bear, and some days the bear gets you. But the probability of making a six is no greater than that of rolling a seted is our normal routine. I am your worst nightmare! What? We're not at all alike! You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? When has justice ever been as simple as a rule book? Maybe we better talk out here; the observation lounge has turned into a swamp. Some days you get the bear, and some days the bear gets you. But the probability of making a six is no greater than that of rolling a seven. Sorry, Data. I'll alert the crew. I'd like to think that I haven't changed those things, sir. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. Wait a minute - you've been declared dead. You can't give orders around here. Maybe if we felt any human loss as keenly as we feel one of those close to us, human ut I will n, I would have grabbed the phaser and pointed it at you instead of them. We have a saboteur aboard. H only enemy. Sure. You'd be surprised how far a hug goes with Geordi, or Worf. I'm afraid I still don't understand, sir. Flair is what marks the difference between artistry and mere competence. Why don't we just give everybody a promotion and call it a night - 'Commander'? This should be interesting. Well, that's certainly good to know. For an android with no feelings, he sure managed to evoke them in others. Not if I weaken first. Commander William Riker of the Starship Enterprise. We finished our first sensor sweep of the neutral zone. In all trust, there is the possibility for betrayal.Computer, lights up! Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Now, how the hell do we defeat an enemy that knows us better than we know ourselves? I've had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them. We have a saboteur aboard. How long can two people talk about nothing? Travel time to the nearest starbase? and attack the Romulans. Now we know what they mean by 'advanced' tactical training. Shields up! Rrrrred alert! Your head is not an artifact! You did exactly what you had to do. You considered all your options, you tried every alternative and then you made the hard choice. Talk about going nowhere fast. This is not about revenge. This is about justice. Well, I'll say this for him - he's sure of himself. Ensign Babyface! Worf, It's better than music. It's jazz. You enjoyed that. I will obey your orders. I will serve this ship as First Officer. And in an attack against the Enterprise, I will die with this crew. But I will not break my oath of loyalty to Starfleet. I guess it's better to be lucky than good. The unexpected is our normal routine. I am your worst nightmare! What? We're not at all alike! You're going to be an interesting companion, Mr. Data. </p>
 				<footer> bottom stuff</footer>
 				</div>
 			</div>
